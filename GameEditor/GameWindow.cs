9d9cb88563e27886deeb0b01f1049fe261363a1b
BillSchumacher <34168009+BillSchumacher@users.noreply.github.com>
2025-05-10 21:13:04 -0500

    initial game editor

---
+++ GameEditor/GameWindow.cs
@@ -0,0 +1,48 @@
+
+using OpenTK.Graphics.OpenGL4;
+using OpenTK.Windowing.Common;
+using OpenTK.Windowing.Desktop;
+using OpenTK.Windowing.GraphicsLibraryFramework;
+using System;
+
+namespace GameEditor
+{
+    public class GameWindow : OpenTK.Windowing.Desktop.GameWindow
+    {
+        public GameWindow(GameWindowSettings gameWindowSettings, NativeWindowSettings nativeWindowSettings)
+            : base(gameWindowSettings, nativeWindowSettings)
+        {
+        }
+
+        protected override void OnLoad()
+        {
+            base.OnLoad();
+            GL.ClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        }
+
+        protected override void OnRenderFrame(FrameEventArgs e)
+        {
+            base.OnRenderFrame(e);
+            GL.Clear(ClearBufferMask.ColorBufferBit);
+
+            // TODO: Add editor rendering logic here
+
+            SwapBuffers();
+        }
+
+        protected override void OnUpdateFrame(FrameEventArgs e)
+        {
+            base.OnUpdateFrame(e);
+            if (KeyboardState.IsKeyDown(Keys.Escape))
+            {
+                Close();
+            }
+        }
+
+        protected override void OnResize(ResizeEventArgs e)
+        {
+            base.OnResize(e);
+            GL.Viewport(0, 0, Size.X, Size.Y);
+        }
+    }
+}
---
+++ GameEditor/Program.cs
@@ -0,0 +1,21 @@
+﻿using OpenTK.Windowing.Desktop;
+
+namespace GameEditor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var nativeWindowSettings = new NativeWindowSettings()
+            {
+                ClientSize = new OpenTK.Mathematics.Vector2i(800, 600), // Changed from Size to ClientSize
+                Title = "Game Editor"
+            };
+
+            using (var window = new GameWindow(GameWindowSettings.Default, nativeWindowSettings))
+            {
+                window.Run();
+            }
+        }
+    }
+}
